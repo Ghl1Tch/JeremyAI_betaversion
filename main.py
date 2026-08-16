f05963805d563bc0dcb936094a29ad0667e5a2f0
Ghl1Tch <148305272+Ghl1Tch@users.noreply.github.com>
2026-08-16 16:29:33 +0300

    install new Listener

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import time
 from core.brain import Brain
 from core.config_loader import load_settings
 from core.memory import Memory
@@ -12,10 +13,14 @@ def main():
 
     memory = Memory(settings["memory_file"])
     brain = Brain(settings, memory)
+
     listener = Listener(settings)
     transcriber = WhisperTranscriber(settings)
     normalizer = TextNormalizer()
     speaker = Speaker(settings)
+
+    active = False
+    active_timeout = 5
 
     print("Jeremy AI Beta 1.0")
     print("Running in background. Say 'Jeremy' to wake him.")
@@ -23,31 +28,55 @@ def main():
 
     try:
         while True:
-            audio_file = listener.wait_for_speech()
+            if active:
+                audio_file = listener.wait_for_speech(
+                    timeout=active_timeout
+                )
+            else:
+                audio_file = listener.wait_for_speech()
 
             if not audio_file:
+                if active:
+                    active = False
+                    print("Jeremy is no longer active.\n")
+
                 continue
 
             text = transcriber.transcribe(audio_file)
 
             if not text:
                 continue
+
             text = normalizer.normalize(text)
+
             print(f"You: {text}")
 
-            if not brain.is_wake_command(text):
-                continue
+            if not active:
+                if not brain.is_wake_command(text):
+                    continue
 
-            command = brain.remove_wake_command(text)
+                active = True
+
+                command = brain.remove_wake_command(text)
+
+                if not command:
+                    speaker.say("Yes?")
+                    continue
+
+            else:
+                command = text
 
             if not command:
-                speaker.say("Yes?")
                 continue
 
             answer = brain.ask(command)
 
             print(f"Jeremy: {answer}")
             speaker.say(answer)
+
+            active = True
+
+            print("Jeremy is active for 5 seconds.\n")
 
     except KeyboardInterrupt:
         print("\nJeremy stopped.")
